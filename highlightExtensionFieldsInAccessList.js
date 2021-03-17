d4090c3f089cd266f6affea2b9c832d2d3138acc
g3ib <sebastian@geib.io>
2021-03-17 11:23:53 +0100

    Update highlightExtensionFieldsInAccessList.js

--- highlightExtensionFieldsInAccessList.js
+++ highlightExtensionFieldsInAccessList.js
@@ -3,10 +3,14 @@
   If you want to add a usergroup for your new extension, apply the key "tx_myext_domain_model" below and all the corresponding accordions will become red or get a red border so that you don't need to expand all fields. It is also useful if you have fields with the same label like title or category.
 */
 
+var myExtension = 'tx_myext_domain_model';
+
+/* typo3-contentIframe is the id of the main iframe in the backend */
 var iframe = document.getElementById('typo3-contentIframe');
+/* get into it */
 var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-var checkboxes = innerDoc.querySelectorAll('input[type="checkbox"][value^="tx_myext_domain_model"]');
-checkboxes.forEach(function(item, index){ 
-	item.closest('tr').style.backgroundColor = 'red'; 
-	item.closest('.panel').style.border = '5px solid red'; 
+
+innerDoc.querySelectorAll('input[type="checkbox"][value^="'+myExtension+'"]').forEach(function(item, index){ 
+    item.closest('tr').style.backgroundColor = 'red'; /* section show / edit tables */
+    item.closest('.panel').style.border = '5px solid red'; /* section non exclude fields */
 });
